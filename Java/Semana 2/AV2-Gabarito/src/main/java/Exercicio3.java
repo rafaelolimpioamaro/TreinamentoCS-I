faa5f3a1925d89a9d5582d31fc4a2bffd1bc3ce6
acJoaog <joao.jg@ges.inatel.br>
2024-06-20 14:54:03 -0300

    fix

--- Java/Semana 2/AV2-Gabarito/src/main/java/Exercicio3.java
+++ Java/Semana 2/AV2-Gabarito/src/main/java/Exercicio3.java
@@ -5,24 +5,25 @@ public class Exercicio3 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("num1 = ");
-        int num1 = sc.nextInt();
-        System.out.print("num2 = ");
-        int num2 = sc.nextInt();
-        System.out.print("num3 = ");
-        int num3 = sc.nextInt();
-
-        int[] array = {num1, num2, num3};
-
-        System.out.print("indice1 = ");
-        int escolha1 = sc.nextInt();
-        System.out.print("indice2 = ");
-        int escolha2 = sc.nextInt();
-
         try {
-            System.out.println("A divisão entre os índices solicitados é: "+ array[escolha1]/array[escolha2]);
-        }catch (Exception e) {
-            System.out.println(e.getMessage());
+
+            System.out.print("num1 = ");
+            int num1 = sc.nextInt();
+            System.out.print("num2 = ");
+            int num2 = sc.nextInt();
+            System.out.print("num3 = ");
+            int num3 = sc.nextInt();
+
+            int[] array = {num1, num2, num3};
+
+            System.out.print("indice1 = ");
+            int escolha1 = sc.nextInt();
+            System.out.print("indice2 = ");
+            int escolha2 = sc.nextInt();
+            System.out.println("A divisão entre os índices solicitados é: " + array[escolha1] / array[escolha2]);
+
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro: "+e.getMessage());
         }
 
         sc.close();
